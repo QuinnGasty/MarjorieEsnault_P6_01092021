f8f3617a4a0171f0ced1f9b0f0652f47001b22d1
Marjorie Esnault <gastyquinn@gmail.com>
2021-09-21 17:57:25 +0200

    Tags index
    Ajout des tags sur l'index HTML/CSS

--- pages/photographer.js
+++ pages/photographer.js
@@ -1,18 +1,24 @@
 const queryString = window.location.search;
-
 const urlParams = new URLSearchParams(queryString);
 const photographerID = urlParams.get("photographerID");
+const userName = document.getElementsByClassName("user-name");
 
 let userData;
 
 const getUser = async () => {
   await fetch("../photographers.json")
     .then((res) => res.json())
-    .then((data) => (userData = data.photographers.filter(
-      p => p.id == photographerID
-    )));
+    .then(
+      (data) =>
+        (userData = data.photographers.filter((p) => p.id == photographerID))
+    );
 
   console.log(userData);
 };
 
-getUser();
+const userDisplay = async () => {
+  await getUser();
+
+};
+
+userDisplay();
