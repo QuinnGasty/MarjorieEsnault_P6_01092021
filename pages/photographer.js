1155a604a08745a576870ced71bc6e762b221abb
Marjorie Esnault <gastyquinn@gmail.com>
2021-09-22 14:50:54 +0200

    JS user pages - cards
    Affichage des informations personnelles de chaque photographe (sauf médias)

--- pages/photographer.js
+++ pages/photographer.js
@@ -1,9 +1,13 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const photographerID = urlParams.get("photographerID");
-const userName = document.getElementsByClassName("user-name");
+const userName = document.getElementById("user-name");
+const userLocation = document.querySelector(".user-location");
+const userTagline = document.querySelector(".user-tagline");
+const userTags = document.querySelector(".user-tags");
+const userID = document.querySelector(".user-id")
 
-let userData;
+let userData = [];
 
 const getUser = async () => {
   await fetch("../photographers.json")
@@ -14,11 +18,19 @@ const getUser = async () => {
     );
 
   console.log(userData);
+
+  userName.textContent += userData[0].name;
+  userLocation.textContent += userData[0].city;
+  userTagline.textContent += userData[0].tagline;
+  userTags.textContent += "#" + userData[0].tags.join(" #");
+
+  userID.innerHTML += `<img
+  src="../images/PhotographersID/${userData[0].portrait}"
+  alt="photo de Ellie Rose Wilkens"
+  width="200px"
+  class="user-pic"
+/>`
 };
 
-const userDisplay = async () => {
-  await getUser();
 
-};
-
-userDisplay();
+getUser();
