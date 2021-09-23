2bd3e2a032779408b0029f29b36789f5cc6c9e76
Marjorie Esnault <gastyquinn@gmail.com>
2021-09-23 12:42:22 +0100

    Fetch media personnal pages
    Récupération et affichage des médias des pages personnelles (sauf vidéos)

--- pages/photographer.js
+++ pages/photographer.js
@@ -5,9 +5,11 @@ const userName = document.getElementById("user-name");
 const userLocation = document.querySelector(".user-location");
 const userTagline = document.querySelector(".user-tagline");
 const userTags = document.querySelector(".user-tags");
-const userID = document.querySelector(".user-id")
+const userID = document.querySelector(".user-id");
+const userPics = document.querySelector(".user-medias")
 
-let userData = [];
+let userData;
+let userMedia;
 
 const getUser = async () => {
   await fetch("../photographers.json")
@@ -16,7 +18,6 @@ const getUser = async () => {
       (data) =>
         (userData = data.photographers.filter((p) => p.id == photographerID))
     );
-
   console.log(userData);
 
   userName.textContent += userData[0].name;
@@ -25,12 +26,48 @@ const getUser = async () => {
   userTags.textContent += "#" + userData[0].tags.join(" #");
 
   userID.innerHTML += `<img
-  src="../images/PhotographersID/${userData[0].portrait}"
-  alt="photo de Ellie Rose Wilkens"
-  width="200px"
-  class="user-pic"
-/>`
+    src="../images/PhotographersID/${userData[0].portrait}"
+    alt="photo de Ellie Rose Wilkens"
+    width="200px"
+    class="user-pic"
+    />`;
 };
 
-
 getUser();
+
+const getMedia = async () => {
+  await fetch("../photographers.json")
+    .then((res) => res.json())
+    .then(
+      (data2) =>
+        (userMedia = data2.media.filter(
+          (m) => m.photographerId == photographerID
+        ))
+    );
+  console.log(userMedia);
+};
+
+getMedia();
+
+const mediaDisplay = async () => {
+  await getMedia();
+
+  userMedia.forEach(media => {
+    userPics.innerHTML += `
+      <div class="media">
+          <img
+            src="../images/${media.photographerId}/${media.image}"
+            alt=""
+            class="media-img"
+          />
+        </div>
+        <div class="media-infos">
+          <small class="media-name">${media.title}</small>
+          <p class="media-likes">
+            ${media.likes} <span class="number-likes"><i class="fas fa-heart"></i></span>
+          </p>
+        </div>`
+  });
+}
+
+mediaDisplay();
