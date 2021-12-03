f5451324b862d995769e81d4596de0c92aaad831
Marjorie Esnault <gastyquinn@gmail.com>
2021-12-03 10:23:52 +0100

    typo html js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json
-
 const app = document.getElementById("app");
 const skipToContent = document.querySelector(".content-banner");
 const photographersTags = document.querySelectorAll(".tags");
